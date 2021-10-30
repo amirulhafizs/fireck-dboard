de0c031c7ca7ac3fe00fa18f6a2cbf5e211b5266
rromikas <rromikas@gmail.com>
2021-10-30 23:47:53 +0300

    update

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -12,7 +12,7 @@ import { clientsClaim } from "workbox-core";
 import { ExpirationPlugin } from "workbox-expiration";
 import { precacheAndRoute, createHandlerBoundToURL } from "workbox-precaching";
 import { registerRoute } from "workbox-routing";
-import { NetworkFirst } from "workbox-strategies";
+import { StaleWhileRevalidate } from "workbox-strategies";
 
 declare const self: ServiceWorkerGlobalScope;
 
@@ -59,7 +59,7 @@ registerRoute(
   // Add in any other file extensions or routing criteria as needed.
   ({ url }) => url.origin === self.location.origin && url.pathname.endsWith(".png"),
   // Customize this strategy as needed, e.g., by changing to CacheFirst.
-  new NetworkFirst({
+  new StaleWhileRevalidate({
     cacheName: "images",
     plugins: [
       // Ensure that once this runtime cache reaches a maximum size the
@@ -77,4 +77,4 @@ self.addEventListener("message", (event) => {
   }
 });
 
-// Any other custom service worker logic can go here
+// Any other custom service worker logic can go here.
